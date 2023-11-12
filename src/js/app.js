7bd10f569afae32bdd9421d69014d9bc08f122dc
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2023-11-12 12:57:32 +0000

    starting js configs

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+let errorMessage = {
+  empty: "This field is required",
+  invalidInput: {
+    day: "Must be a valid day",
+    month: "Must be a valid month",
+    year: "Must be a valid year",
+  },
+}
+
+let birthdayInfo = {
+  day,
+  month,
+  year,
+}
+
+const inputField = document.querySelectorAll("input[type=number]");
+const confirmBtn = document.getElementById("active-btn");
+
+confirmBtn.addEventListener("click", () => {
+
+})
+
+
